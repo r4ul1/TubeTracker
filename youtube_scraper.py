1dd8fbde1cb8f5a3fe15dcf6cbb363883982f898
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-01-26 15:45:14 +0100

    er-diagramm

--- youtube_scraper.py
+++ youtube_scraper.py
@@ -15,8 +15,10 @@ def get_subscriber_count(channel_id):
     url = f"https://www.youtube.com/channel/{channel_id}"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
     response = requests.get(url, headers=headers)
+    print(f"Response status code: {response.status_code}")
     soup = BeautifulSoup(response.text, "html.parser")
     subscriber_count = soup.find("subscriber-count", {"class=":"meta-item style-scope ytd-c4-tabbed-header-renderer"})
+    subscriber_count = soup.select("span[class*='subscriber-count']")
     if subscriber_count:
         return subscriber_count.text
     else:
