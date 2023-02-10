94a25244dcd1a2e57d31ba6a6509d7a6b16624e9
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-02-10 07:30:49 +0100

    code comments :)

--- youtube_gui.py
+++ youtube_gui.py
@@ -3,11 +3,13 @@ import sqlite3
 import datetime
 import youtube_scraper
 
-
+# Function to create the database 'youtube_database.db' if it does not exist.
 def create_database():
+    # Connect to the database.
     conn = sqlite3.connect("youtube_database.db")
     cursor = conn.cursor()
 
+    # Create the table 'youtubers' if it does not exist.
     cursor.execute("""CREATE TABLE IF NOT EXISTS youtubers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
@@ -15,36 +17,53 @@ def create_database():
             timestamp TEXT)
         """)
 
+    # Confirm that the database was created successfully.
     print("Database created successfully.")
     conn.close()
 
-
+# Function to add/update the data of a YouTube channel in the database.
 def add_to_database(name, subscribers):
+    # Connect to the database.
     conn = sqlite3.connect("youtube_database.db")
     cursor = conn.cursor()
+
+    # Create the table 'youtubers' if it does not exist.
     cursor.execute("CREATE TABLE IF NOT EXISTS youtubers (name text, subscribers integer, timestamp datetime DEFAULT CURRENT_TIMESTAMP)")
 
+    # Get the current timestamp.
     timestamp = str(datetime.datetime.now())
 
+    # Check if the channel already exists in the database.
     cursor.execute("SELECT * FROM youtubers WHERE name=?", (name,))
     data = cursor.fetchall()
+
+    # If the channel does not exist, insert the data.
     if len(data) == 0:
         cursor.execute("""INSERT INTO youtubers (name, subscribers, timestamp)
                         VALUES (?,?,?)
                     """, (name, subscribers, timestamp))
+    # If the channel already exists, update the data.
     else:
         cursor.execute("""UPDATE youtubers SET subscribers=?, timestamp=? WHERE name=?
                     """, (subscribers, timestamp, name))
 
+    # Commit the changes and close the connection to the database.
     conn.commit()
     print("Data added/updated successfully.")
     conn.close()
 
-
+# Function to search for the number of subscribers of a YouTube channel using the YouTube scraper.
 def search_yt():
+    # Get the channel name entered by the user.
     name = entry.get()
+    
+    # Get the number of subscribers using the YouTube scraper.
     subscribers = youtube_scraper.get_sub_count(name)
+
+    # Add/update the data of the channel in the database.
     add_to_database(name, subscribers)
+    
+    # Update the result label with the number of subscribers of the channel.
     result_label.config(text=f"{name} has {subscribers} subscribers.")
 
 root = tk.Tk()
--- youtube_scraper.py
+++ youtube_scraper.py
@@ -3,20 +3,27 @@ import json
 from bs4 import BeautifulSoup, Tag
 
 
+# The function retrieves the number of subscribers of a YouTube channel by searching for the channel's name on YouTube's search page.
 def get_sub_count(name):
     url = f"https://www.youtube.com/results?search_query={name}"
+# It uses the requests library to make a GET request to the search page and BeautifulSoup to parse the HTML content.
     response = requests.get(url)
     soup = BeautifulSoup(response.content, features="html.parser")
+# The 33rd script tag in the page's source code is found and the relevant information is extracted from the JSON data within the tag.
     scripts: Tag = soup.findAll("script")[33]
     scripts = scripts.get_text().split(" = ", maxsplit=1)[1].rsplit(";", maxsplit=1)[0]
     scripts: dict = json.loads(scripts)
     scripts = scripts["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"]
     for c in range(len(scripts)):
         try:
+# The function loops through the contents of the scripts data and tries to extract the subscriber count string in the format of "number unit", such as "111 Mio.".
             subscriber_string = scripts[c]["itemSectionRenderer"]["contents"][0]["channelRenderer"]["videoCountText"]["simpleText"]
+# The subscriber count string is then split into two parts: the number and the unit.
             number, unit = subscriber_string.split(" ")
+# If there is a dot in the number, the dot is removed. Finally, the function returns the subscriber count as an integer.
             if '.' in number:
                 number = number.replace('.', '')
+# If the unit is "Mio", the number is converted to an integer by multiplying it by 1000000.
             if unit == "Mio.":
                 number = int(float(number) * 1000000)
             return number
